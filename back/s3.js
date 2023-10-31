e16b9da45808743b10a2632155d59660abc7f4fd
Damian Galeano <dj.damian.b@hotmail.com>
2023-10-31 15:18:33 +0100

    Create env and add S3 to connect keys

--- back/s3.js
+++ back/s3.js
@@ -1,22 +1,22 @@
-// import dotenv from 'dotenv'
-// import aws from 'aws-sdk'
+import dotenv from 'dotenv'
+import aws from 'aws-sdk'
 // import crypto from 'crypto'
 // import { promisify } from "util"
 // const randomBytes = promisify(crypto.randomBytes)
 
 // dotenv.config()
 
-// const region = "us-west-2"
-// const bucketName = "direct-upload-s3-bucket-thing"
-// const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const region = "us-west-2"
+const bucketName = "s3-react-shoes-img"
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
-// const s3 = new aws.S3({
-//   region,
-//   accessKeyId,
-//   secretAccessKey,
-//   signatureVersion: 'v4'
-// })
+const s3 = new aws.S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+  signatureVersion: 'v4'
+})
 
 // export async function generateUploadURL() {
 //   const rawBytes = await randomBytes(16)
